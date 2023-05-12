e802c38fd89008c04376c7a9386f123f54359938
MPrimo8 <matyprimo@hotmail.com>
2023-05-12 17:01:04 -0300

    step-13

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_8_mprime8/constants/routes.dart';
 import 'package:flutter_application_8_mprime8/services/auth/auth_service.dart';
 import 'package:flutter_application_8_mprime8/views/login_view.dart';
-import 'package:flutter_application_8_mprime8/views/notes_view.dart';
+import 'package:flutter_application_8_mprime8/views/notes/new_note_view.dart';
+import 'package:flutter_application_8_mprime8/views/notes/notes_view.dart';
 import 'package:flutter_application_8_mprime8/views/register_view.dart';
 import 'package:flutter_application_8_mprime8/views/verify_email_view.dart';
 
@@ -19,6 +20,7 @@ void main() {
       registerRoute: (context) => const RegisterView(),
       notesRoute: (context) => const NotesView(),
       verifyEmailRoute: (context) => const VerifyEmailView(),
+      newNoteRoute: (context) => const NewNoteView(),
     },
   ));
 }
--- test/auth_test.dart
+++ test/auth_test.dart
@@ -101,7 +101,7 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'abc@abc.com') throw UserNotFoundAuthException();
     if (password == 'abcabc') throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false);
+    const user = AuthUser(isEmailVerified: false, email: 'user');
     _user = user;
     return Future.value(user);
   }
@@ -119,7 +119,7 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true);
+    const newUser = AuthUser(isEmailVerified: true, email: 'user');
     _user = newUser;
   }
 }
